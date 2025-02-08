fadf8364e357b2e9b935629eef5dad0c78b4d7b9
Soman <ahmad@soman.ca>
2025-02-08 17:46:03 -0500

    Intial commit

---
+++ InventoryMgmt/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InventoryMgmt.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
